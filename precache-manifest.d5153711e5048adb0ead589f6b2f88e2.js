07503354168ff8709d9266c544e01c3d391d892d
rowshenpaltayev <rowshenpaltayev@gmail.com>
2019-02-21 16:18:35 -0600

    Updates

---
+++ precache-manifest.d5153711e5048adb0ead589f6b2f88e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09b9dc748cdcc1bd1570",
+    "url": "/rpaltayev/EOG-Assesment-React-Visualization.git/static/js/runtime~main.09b9dc74.js"
+  },
+  {
+    "revision": "7e833dd3ede7b1b391fd",
+    "url": "/rpaltayev/EOG-Assesment-React-Visualization.git/static/js/main.7e833dd3.chunk.js"
+  },
+  {
+    "revision": "b79d1b91a416a9bbbc8b",
+    "url": "/rpaltayev/EOG-Assesment-React-Visualization.git/static/js/1.b79d1b91.chunk.js"
+  },
+  {
+    "revision": "b79d1b91a416a9bbbc8b",
+    "url": "/rpaltayev/EOG-Assesment-React-Visualization.git/static/css/1.297a6a09.chunk.css"
+  },
+  {
+    "revision": "e6f42fabfada0de3003913df51213718",
+    "url": "/rpaltayev/EOG-Assesment-React-Visualization.git/index.html"
+  }
+];
